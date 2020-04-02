3a9721da7819200ff7623eaa76086342865748f9
Julio <july2m3@gmail.com>
2020-04-01 22:53:34 -0500

    Updates

---
+++ precache-manifest.028a27602cd20f6ef95fbbde5d79da78.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10e31e48115298957a1794469b966be4",
+    "url": "/pomodoro-react/index.html"
+  },
+  {
+    "revision": "416551b824048224fba3",
+    "url": "/pomodoro-react/static/css/main.35e5cbec.chunk.css"
+  },
+  {
+    "revision": "2cf882cb4eceb3e90d00",
+    "url": "/pomodoro-react/static/js/2.d743bdb1.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/pomodoro-react/static/js/2.d743bdb1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "416551b824048224fba3",
+    "url": "/pomodoro-react/static/js/main.463a73c4.chunk.js"
+  },
+  {
+    "revision": "bab315cc65a37b348ff1",
+    "url": "/pomodoro-react/static/js/runtime-main.c1f360ae.js"
+  },
+  {
+    "revision": "99305aca99b2d20d30ab272aea709aed",
+    "url": "/pomodoro-react/static/media/dark.99305aca.jpeg"
+  },
+  {
+    "revision": "7087c8c471e582b1ca2f2ace196583c6",
+    "url": "/pomodoro-react/static/media/gong.7087c8c4.mp3"
+  },
+  {
+    "revision": "df10a365223eae53545cdb2b68980a6e",
+    "url": "/pomodoro-react/static/media/mountains.df10a365.jpg"
+  }
+]);
